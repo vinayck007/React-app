3602c83fcb2d2f6d34bb9accc2d28f96b7a3a06b
vinayck007 <vinayck007@gmail.com>
2023-11-07 12:01:01 +0530

    Passing Data
    Passing Data between components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 4, 30)
-  const expenseTitle = 'Bike Insurance'
-  const expenseAmount = 124.00
-  const locationOfExpenditure = 'Hotel'
-
+function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">  
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__description">
-        <h2>{locationOfExpenditure}</h2>
-      <div className="expense-item__amount">{expenseAmount}</div>
+        <h2>{props.location}</h2>
+      <div className="expense-item__amount">{props.amount}</div>
     </div>
     </div>
     </div>
