c73644bdde9385c4471b98b8199a2a6f89eaa831
vinayck007 <vinayck007@gmail.com>
2023-11-07 11:26:00 +0530

    LocationOfExpenditure
    Adding Dynamic Variable to JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-  return <><h2>Expense Item</h2><h3>Food Rs 10</h3><h3>Petrol Rs 100</h3><h3>Movies Rs 200</h3></>
-}
+  const expenseDate = new Date(2023, 4, 30)
+  const expenseTitle = 'Bike Insurance'
+  const expenseAmount = 124.00
+  const locationOfExpenditure = 'Hotel'
+
+  return (
+    <div className="expense-item">  
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__description">
+        <h2>{locationOfExpenditure}</h2>
+      <div className="expense-item__amount">{expenseAmount}</div>
+    </div>
+    </div>
+    </div>
+  )
+  }
 
 export default ExpenseItem
