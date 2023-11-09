a0daddc6ab53d6c977f357c14a4c445439cfd958
vinayck007 <vinayck007@gmail.com>
2023-11-09 14:49:05 +0530

    Change amount to $100
    using useState

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,18 +2,21 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
+import React, { useState } from 'react';
 
 const ExpenseItem = (props) => {
 
-  const deleteHandler = () => {
-    props.onDelete(props.id)
-  }
+  const [amount, setAmount] = useState(props.amount)
 
+  const amountHandler = () => {
+    setAmount('$100')
+    console.log(amount)
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-      <button onClick={deleteHandler}>Delete</button>
+      <ExpenseDetails amount={amount}  location={props.location}   title={props.title} />
+      <button onClick={amountHandler}>Edit Amount</button>
     </Card>
   );
 }
