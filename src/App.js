654beb3b2c878e818a52fa105bc8ff5bbc4490fe
vinayck007 <vinayck007@gmail.com>
2023-11-09 13:39:51 +0530

    Delete Expense
    Using Event Handler

--- src/App.js
+++ src/App.js
@@ -1,61 +1,60 @@
-import ExpenseItem from './components/Expense/ExpenseItem'
+import ExpenseItem from './components/Expense/ExpenseItem';
+import React, { useState } from 'react';
 
 const App = () => {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      locationOfExpenditure: 'Hotel'
+      locationOfExpenditure: 'Shop'
     },
-    { id: 'e2', 
-    title: 'New TV', 
-    amount: 799.49, 
-    date: new Date(2021, 2, 12),
-    locationOfExpenditure: 'Hyper Market'
-  },
-    
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: 'Hyper Market'
+    },
     {
       id: 'e3',
-      title: 'Car Insurance',
+      title: 'Movie',
       amount: 294.67,
       date: new Date(2021, 2, 28),
       locationOfExpenditure: 'Theatre'
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'New York',
       amount: 450,
       date: new Date(2021, 5, 12),
       locationOfExpenditure: 'Travel'
     },
-  ];
+  ]);
 
-  
-  const expenseItems = [];
+  const deleteExpenseHandler = (id) => {
+    const updatedExpenses = expenses.filter((expense) => expense.id !== id);
+    setExpenses(updatedExpenses);
+  };
 
-  for (let i = 0; i < expenses.length; i++) {
-    const expense = expenses[i];
-    expenseItems.push(
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        location={expense.locationOfExpenditure}
-      />
-    );
-  }
+  const expenseItems = expenses.map((expense) => (
+    <ExpenseItem
+      id={expense.id} 
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+      location={expense.locationOfExpenditure}
+      onDelete={deleteExpenseHandler}
+    />
+  ));
 
   return (
     <div>
-    <h2>Let's get started!</h2>
-    {expenseItems}
-    
-  </div>
+      <h2>Let's get started!</h2>
+      {expenseItems}
+    </div>
   );
-}
+};
 
 export default App;
-
-
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -4,10 +4,16 @@ import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+  const deleteHandler = () => {
+    props.onDelete(props.id)
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+      <button onClick={deleteHandler}>Delete</button>
     </Card>
   );
 }
