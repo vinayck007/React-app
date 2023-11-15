1ad68af31f5c65976ceb35f8c37bac6d0cbbd8c2
vinayck007 <vinayck007@gmail.com>
2023-11-15 11:04:50 +0530

    Hide expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,6 +7,9 @@ const ExpenseForm = (props) => {
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredLocation, setEnteredLocation] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
+  const [formVisible, setFormVisible] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -40,23 +43,37 @@ const ExpenseForm = (props) => {
     setEnteredLocation('')
     setEnteredAmount('')
     setEnteredDate('')
+    setFormVisible(false);
+    setFormSubmitted(true);
+
+  };
+
+  const showForm = () => {
+    setFormVisible(true);
+    setFormSubmitted(false);
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <div className='new-expense__controls'>
+      {!formVisible && !formSubmitted && (
+        <button onClick={showForm}>Add New Expense</button>
+      )}
+
+      {formVisible && (
+      <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input 
-          type='text' 
-          value={enteredTitle}
-          onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Location</label>
-          <input type='text' 
-          value={enteredLocation}
-          onChange={locationChangeHandler} />
+          <input type='text'
+            value={enteredLocation}
+            onChange={locationChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -65,8 +82,7 @@ const ExpenseForm = (props) => {
             min='0.01'
             step='0.01'
             value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+            onChange={amountChangeHandler} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
@@ -75,14 +91,19 @@ const ExpenseForm = (props) => {
             min='2019-01-01'
             max='2022-12-31'
             value={enteredDate}
-            onChange={dateChangeHandler}
-          />
+            onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
+      )}
+
+      {formSubmitted && (
+        <p>Expense submitted! <button onClick={showForm}>Add Another Expense</button></p>
+      )}
+    </div>
   );
 };
 
