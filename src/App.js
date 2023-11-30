b6102cf7e054484d5493c8009e15f219d4baaafa
vinayck007 <vinayck007@gmail.com>
2023-11-30 13:09:10 +0530

    Food Order App
    Header Design

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
+import { Fragment } from 'react';
 
-import React, { useState, useEffect } from 'react';
-import ProductList from './components/AddProduct';
-import TotalPrice from './components/TotalPrice';
+import Header from './components/Layout/Header';
 
-const App = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const storedProducts = JSON.parse(localStorage.getItem('products')) || [];
-    setProducts(storedProducts);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('products', JSON.stringify(products));
-  }, [products]);
-
-  const addProduct = (product) => {
-    setProducts([...products, product]);
-  };
-
-  const deleteProduct = (id) => {
-    const updatedProducts = products.filter((product) => product.id !== id);
-    setProducts(updatedProducts);
-  };
-
+function App() {
   return (
-    <div className='products'>
-      <h1>Product Management</h1>
-      <ProductList products={products} addProduct={addProduct} deleteProduct={deleteProduct} />
-      <TotalPrice products={products} />
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
-};
+}
 
 export default App;
