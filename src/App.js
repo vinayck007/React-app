ddbe7e5600b3e49bdbbba96c8880c2f93c158e79
vinayck007 <vinayck007@gmail.com>
2023-12-08 15:54:10 +0530

    Add Movie Input Field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import Loader from './components/Loader';
+import MovieForm from './components/MovieForm';
+
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [retryInterval, setRetryInterval] = useState(null);
   const [error, setError] = useState(null);
+  const [newMovie, setNewMovie] = useState({ title: '', openingText: '', releaseDate: '' });
+
 
   const retry = useCallback(() => {
     setRetryInterval(setInterval(fetchMoviesHandler, 5000));
@@ -52,9 +56,29 @@ function App() {
     fetchMoviesHandler(); // Initial fetch
   }, [fetchMoviesHandler]);
 
+  const addMovieHandler = (newMovieData) => {
+    // Add the new movie to the list with a unique id (using timestamp)
+    const newMovie = {
+      id: Date.now(),
+      title: newMovieData.title,
+      openingText: newMovieData.openingText,
+      releaseDate: newMovieData.releaseDate,
+    };
+  
+    // Update the local state immediately
+    setMovies((prevMovies) => [...prevMovies, newMovie]);
+
+    console.log('Added Movie:', newMovie);
+
+    setNewMovie({ title: '', openingText: '', releaseDate: '' });
+  }
+  
+
   return (
     <React.Fragment>
       <section>
+      <MovieForm onAddMovie={addMovieHandler} />
+
         <button onClick={() => fetchMoviesHandler()}>
           Fetch Movies
         </button>
