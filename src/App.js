7954cbbb21ee80a15fd3302bfd411c238a97f8bc
vinayck007 <vinayck007@gmail.com>
2023-12-08 13:10:45 +0530

    Error Handling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import MoviesList from './components/MoviesList';
-import Loader from './components/Loader'
+import Loader from './components/Loader';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isRetrying, setIsRetrying] = useState(false);
+  const [error, setError] = useState(null);
+  const [fetchClicked, setFetchClicked] = useState(false);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    setError(null);
+
     try {
-      setIsLoading(true);
       const response = await fetch('https://swapi.dev/api/films/');
       const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error('Something went wrong ... Retrying...');
+      }
 
       const transformedMovies = data.results.map((movieData) => ({
         id: movieData.episode_id,
@@ -24,19 +32,39 @@ function App() {
       setMovies(transformedMovies);
     } catch (error) {
       console.error('Error fetching movies:', error);
+      setError(error.message);
+      setIsRetrying(true);
+
+      setTimeout(() => {
+        setIsRetrying(false);
+        fetchMoviesHandler();
+      }, 5000);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (fetchClicked) {
+      fetchMoviesHandler();
+      setFetchClicked(false);
+    }
+  }, [fetchClicked]);
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={() => setFetchClicked(true)} disabled={isRetrying}>
+          Fetch Movies
+        </button>
       </section>
       <section>
         {isLoading && <Loader />}
-        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && error && <p>{error}</p>}
+        {!isLoading && movies.length === 0 && !error && !isRetrying && (
+          <p>No Movies Found!</p>
+        )}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
       </section>
     </React.Fragment>
   );
